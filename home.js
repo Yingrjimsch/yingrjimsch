6184c53f0cb0005c8320aede95c9a527aec98195
Yingrjimsch <nobel98@bluewin.ch>
2023-04-17 21:44:39 +0200

    chore: added index for bio and projects

--- home.js
+++ home.js
@@ -1,11 +1,31 @@
-const backgroundSymbols = '°~^/*-+☰☱☲☳☴☵☶☷☣☄☆☼'
+const randChar = generateRandomUtf8([1,2,3,4])
 
 window.onload = function () {
     const bg = document.getElementById('background');
-    const backgroundSymbol = backgroundSymbols[getRndInteger(0, backgroundSymbols.length - 1)];
-    bg.textContent = Array(10000).join('   ' + Array(getRndInteger(6, 10)).join(backgroundSymbol));
+    bg.textContent = Array(10000).join('   ' + Array(getRndInteger(6, 10)).join(randChar));
 }
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// copied from https://onlineutf8tools.com/generate-random-utf8
+function generateRandomUtf8(g) {
+    function i(a, o) {
+        return Math.floor(Math.random() * (o - a + 1)) + a
+    }
+    function v(a) {
+        return a[parseInt(Math.random() * a.length)]
+    }
+    var r = v(g);
+    if (r == 1)
+        var t = i(32, 126);
+    else if (r == 2)
+        var t = i(128, 2047);
+    else if (r == 3)
+        var t = i(2048, 65535);
+    else if (r == 4)
+        var t = i(65536, 1114111);
+    var l = String.fromCodePoint(t);
+    return l
 }
