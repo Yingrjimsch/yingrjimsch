abdb3b532c1fd2070f587f3fb7ea58be77bd8c65
Yingrjimsch <nobel98@bluewin.ch>
2023-04-18 16:33:12 +0200

    chore: refactored css files

--- home.js
+++ home.js
@@ -1,31 +1,23 @@
-const randChar = generateRandomUtf8([1,2,3,4])
+const randChar = generateRandomUtf8(randomInt(1,4))
 
 window.onload = function () {
     const bg = document.getElementById('background');
-    bg.textContent = Array(10000).join('   ' + Array(getRndInteger(6, 10)).join(randChar));
+    bg.textContent = Array(10000).join('   ' + Array(randomInt(6, 10)).join(randChar));
 }
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// copied from https://onlineutf8tools.com/generate-random-utf8
-function generateRandomUtf8(g) {
-    function i(a, o) {
-        return Math.floor(Math.random() * (o - a + 1)) + a
-    }
-    function v(a) {
-        return a[parseInt(Math.random() * a.length)]
-    }
-    var r = v(g);
-    if (r == 1)
-        var t = i(32, 126);
-    else if (r == 2)
-        var t = i(128, 2047);
-    else if (r == 3)
-        var t = i(2048, 65535);
-    else if (r == 4)
-        var t = i(65536, 1114111);
-    var l = String.fromCodePoint(t);
-    return l
+// with help of: https://onlineutf8tools.com/generate-random-utf8
+function generateRandomUtf8(byte) {
+    if (byte == 1)
+        var t = randomInt(32, 126);
+    else if (byte == 2)
+        var t = randomInt(128, 2047);
+    else if (byte == 3)
+        var t = randomInt(2048, 65535);
+    else if (byte == 4)
+        var t = randomInt(65536, 1114111);
+    return String.fromCodePoint(t);
 }
